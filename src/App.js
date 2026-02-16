8da0951ba8dfa36f60b28fedc93abb9036631d27
Calvin Bagby <cbagby33@gmail.com>
2026-02-16 18:12:06 -0500

    split drum machine component into separate components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,82 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function DrumMachineLogo(){
+  return(
+    <div id="logo">
+      <div id="logo-inner">
+        <div className="logo-text logo-text-one">BIG WEB</div>
+        <div className="logo-text logo-text-two">Drum Machine</div>
+        <div className="logo-text logo-text-three">1000</div>
+      </div>
+    </div>
+  )
+}
+class Slider extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div className="slider-tray">
+        <div className="slider"></div>
+      </div>
+    )
+  }
+}
+class SwitchController extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div className="switch">
+        <div className="switch-title">Switch</div>
+        <div className="switch-control">
+          <div className="switch-button"></div>
+        </div>
+      </div>
+    )
+  }
+}
+class DrumPads extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div id="drum-pads">
+        <div className="pad-row">
+          <DrumPad />
+          <DrumPad />
+          <DrumPad />
+        </div>
+        <div className="pad-row">
+          <DrumPad />
+          <DrumPad />
+          <DrumPad />
+        </div>
+        <div className="pad-row">
+          <DrumPad />
+          <DrumPad />
+          <DrumPad />
+        </div>
+      </div>
+    )
+  }
+}
+
+class DrumPad extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div className="drum-pad"></div>
+    )
+  }
+}
+
 class DrumMachine extends React.Component{
   constructor(props){
     super(props)
@@ -11,58 +87,19 @@ class DrumMachine extends React.Component{
     return(
       <div id='drum-machine'>
         <div className="side side-one">
-          <div id="logo">
-            <div id="logo-inner">
-              <div className="logo-text logo-text-one">BIG WEB</div>
-              <div className="logo-text logo-text-two">Drum Machine</div>
-              <div className="logo-text logo-text-three">1000</div>
-            </div>
-          </div>
-          <div id="drum-pads">
-            <div className="pad-row">
-              <div className="drum-pad"></div>
-              <div className="drum-pad"></div>
-              <div className="drum-pad"></div>
-            </div>
-            <div className="pad-row">
-              <div className="drum-pad"></div>
-              <div className="drum-pad"></div>
-              <div className="drum-pad"></div>
-            </div>
-            <div className="pad-row">
-              <div className="drum-pad"></div>
-              <div className="drum-pad"></div>
-              <div className="drum-pad"></div>
-            </div>
-          </div>
+          <DrumMachineLogo />
+          <DrumPads />
         </div>
         <div className="side side-two">
           <div id="display"></div>
           <div id="controls">
             <div className="control-section" id="switches">
-              <div className="switch">
-                <div className="switch-title">Switch</div>
-                <div className="switch-control">
-                  <div className="switch-button"></div>
-                </div>
-              </div>
-              <div className="switch">
-                <div className="switch-title">Switch</div>
-                <div className="switch-control">
-                  <div className="switch-button"></div>
-                </div>
-              </div>
-              <div className="switch">
-                <div className="switch-title">Switch</div>
-                <div className="switch-control">
-                  <div className="switch-button"></div>
-                </div>
-              </div>
+              <SwitchController />
+              <SwitchController />
+              <SwitchController />
             </div>
             <div className="control-section" id="sliders">
-              <div className="slider-tray">
-                <div className="slider"></div>
-              </div>
+              <Slider />
             </div>
           </div>
         </div>
