4d8ee628b1d3594b3033e06af3b22e9c049ca3f0
Calvin Bagby <cbagby33@gmail.com>
2026-02-18 05:10:09 -0500

    moved audio source info to JSON file

--- src/App.js
+++ src/App.js
@@ -3,100 +3,10 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const audioSources = {
-  triggerKeys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'],
-  bankName:['Heater Kit', 'Smooth Piano Kit'],
-  Q: {
-    'bank-1':{
-      'pad-name': 'Heater 1',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3'
-      },
-    'bank-2':{
-      'pad-name': 'Chord 1',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-    }
-  },
-  W: {
-    'bank-1':{
-      'pad-name': 'Heater 2',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Chord 2',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-    }
-  },
-  E: {
-    'bank-1':{
-      'pad-name': 'Heater 3',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Chord 3',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-    }
-  },
-  A: {
-    'bank-1':{
-      'pad-name': 'Heater 4',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Shaker',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-    }
-  },
-  S: {
-    'bank-1':{
-      'pad-name': 'Clap',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Open HH',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-    }
-  },
-  D: {
-    'bank-1':{
-      'pad-name': 'Open Hi-Hat',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Closed HH',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-    }
-  },
-  Z: {
-    'bank-1':{
-      'pad-name': 'Kick & Hat',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Punchy Kick',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-    }
-  },
-  X: {
-    'bank-1':{
-      'pad-name': 'Kick',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Rimshot',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-    }
-  },
-  C: {
-    'bank-1':{
-      'pad-name': 'Closed Hi-Hat',
-      'audio-src': 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'
-    },
-    'bank-2':{
-      'pad-name': 'Snare',
-      'audio-src': 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-    }
-  }
-}
+import jsonData from './audioSources.json'
+
+// object contains audio source information and trigger keys for drumpads
+const audioSources = JSON.parse(jsonData);
 
 function DrumMachineLogo(){
   return(
