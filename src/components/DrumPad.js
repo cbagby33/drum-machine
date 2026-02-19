5ad00476198eba5f71cab3303dbe6740b589ed3e
Calvin Bagby <cbagby33@gmail.com>
2026-02-18 19:35:09 -0500

    added active drumpad functionality to component state

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -4,7 +4,16 @@ import React from 'react';
 class DrumPad extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      bgColor: '#808080',
+      fontWeight: 'normal',
+      textColor: 'black',
+      height: '70px',
+      boxShadow:'black 3px 3px 5px'
+    }
     this.clickHandler = this.clickHandler.bind(this)
+    this.showActiveState = this.showActiveState.bind(this)
+    this.hideActiveState = this.hideActiveState.bind(this)
     this.handleKeyPress = this.handleKeyPress.bind(this)
   }
   componentDidMount() {
@@ -16,11 +25,32 @@ class DrumPad extends React.Component{
   componentDidUpdate() {
     document.getElementById(this.props.padId).volume = this.props.volume;
   }
+  hideActiveState(){
+    this.setState({
+      bgColor: '#808080',
+      fontWeight: 'normal',
+      textColor: 'black',
+      height: '70px',
+      boxShadow:'black 3px 3px 5px'
+    });
+  }
+  showActiveState(){
+    this.setState({
+      bgColor: 'red',
+      fontWeight: 'bold',
+      textColor: 'white',
+      height: '67px',
+      boxShadow:'red 0px 3px'
+    });
+  }
   clickHandler(){
     if(this.props.enabled){
       document.getElementById(this.props.padId).currentTime = 0;
       document.getElementById(this.props.padId).play();
-      this.props.onDisplayChange(this.props.bank['pad-name'])
+      this.props.onDisplayChange(this.props.bank['pad-name']);
+
+      this.showActiveState();
+      setTimeout(this.hideActiveState, 100)
     }
   }
   handleKeyPress(e){
@@ -30,7 +60,14 @@ class DrumPad extends React.Component{
   }
   render(){
     return(
-      <div className="drum-pad" id={this.props.bank['pad-name']} onMouseDown={this.clickHandler} >
+      <div className="drum-pad" id={this.props.bank['pad-name']} onMouseDown={this.clickHandler} style={
+        {
+          backgroundColor: `${this.state.bgColor}`, 
+          fontWeight: `${this.state.fontWeight}`, 
+          color: `${this.state.textColor}`, 
+          height: `${this.state.height}`, 
+          boxShadow: `${this.state.boxShadow}`
+        }}>
         {this.props.padId}
         <audio className="clip" id={this.props.padId} src={this.props.bank['audio-src']}></audio>
       </div>
